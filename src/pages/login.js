65a658daf0745fce170bad1272bfbda05c8ae5f0
Iago Pinheiro <iagoo.pinheiro73@gmail.com>
2026-03-02 20:25:13 -0300

    aula03: api publica e página de login
    - consumindo api pública
    - página de login
    - estilização
    - configuração de rotas e App.js

---
+++ src/pages/login.js
@@ -0,0 +1,67 @@
+import React, {useState} from 'react'
+import { StyleSheet, Text, TextInput, Touchable, TouchableOpacity, Alert, View } from 'react-native'
+import { useNavigation } from '@react-navigation/native'
+
+const Login = () => {
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  const navigation = useNavigation()
+
+  const handleLogin = () => {
+    if(email === "teste@teste.com" && password === "1234"){
+      navigation.navigate("main")
+    } else{
+      Alert.alert("E-mail ou senha inválidos!")
+    }
+  }
+
+return(
+ <View style={styles.container}>
+    <TextInput
+      style={styles.input}
+      placeholder='Email'
+      value={email}
+      onChangeText={setEmail}
+    />
+    <TextInput
+      style={styles.input}
+      placeholder='Password'
+      value={password}
+      onChangeText={setPassword}
+      secureTextEntry={true}
+    />
+    <TouchableOpacity style={styles.button} onPress={() => handleLogin(email, password)}>
+      <Text style={styles.buttonText} >LOGIN</Text>
+    </TouchableOpacity>
+ </View> 
+)
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    padding: 10,
+    marginVertical: 10,
+  },
+  button: {
+    backgroundColor: "#9705f9c2",
+    borderRadius: 5,
+    padding: 10,
+    width: "80%",
+    alignItems: "center"
+  },
+  buttonText:{
+    color: "#fff"
+  }
+})
+
+export default Login
+  
---
+++ src/pages/main.js
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import {Keyboard, ActivityIndicator} from "react-native"
+import Icon from "@expo/vector-icons/MaterialIcons"
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import Api from "../services/api"
+import {} from "../styles"
+
+export default class Main extends Component {
+
+}
